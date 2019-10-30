c90cd25b0122bc733a3b91fe91a8832436c31262
drwm-base <david.medeiros@live.com>
2019-10-30 19:23:00 -0400

    added twitter:site

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -70,6 +70,10 @@ function SEO({
           name: 'twitter:image',
           content: 'https://raw.githubusercontent.com/openclimatefix/website/master/src/images/logo_dark_square%402x.png',
         },
+        {
+          name: 'twitter:site',
+          content: '@OpenClimateFix',
+        },
       ].concat(meta)}
     />
   );
