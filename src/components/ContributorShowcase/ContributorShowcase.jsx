42a69d2a37114fd062b1796e31689f3d2a378905
drwm-base <david.medeiros@live.com>
2019-11-14 21:05:42 -0500

    fixed lint issues

--- src/components/ContributorShowcase/ContributorShowcase.jsx
+++ src/components/ContributorShowcase/ContributorShowcase.jsx
@@ -1,42 +1,40 @@
+import { StaticQuery, graphql } from 'gatsby';
 import React from 'react';
-
 import styles from './ContributorShowcase.module.css';
-import {StaticQuery, graphql} from 'gatsby';
+
+function getContributors(data) {
+  const cont = [];
+  data.contributorsJson.contributors.forEach((item) => {
+    cont.push(
+      <div className={styles.griditem} key={item.name}>
+        <img className={styles.image} alt={item.profile} src={item.avatar_url} />
+        <a href={item.profile}>{item.name}</a>
+      </div>,
+    );
+  });
+  return cont;
+}
 
 function ContributorShowcase() {
   return (
     <div>
       <h2>Contributors</h2>
-      <br></br>
-      <StaticQuery 
-      query = {graphql`
-        query queryJSON {
-          contributorsJson {
-            contributors {
-              name
-              profile
-              avatar_url
+      <br />
+      <StaticQuery
+        query={graphql`
+          query queryJSON {
+            contributorsJson {
+              contributors {
+                name
+                profile
+                avatar_url
+              }
             }
           }
-        }
-      `}
-      render={data => <div className={styles.grid}>{getContributors(data)}</div>}
-    />
+        `}
+        render={(data) => <div className={styles.grid}>{getContributors(data)}</div>}
+      />
     </div>
   );
-} 
-
-
-function getContributors(data) {
-  const cont = [];
-  data.contributorsJson.contributors.forEach(item => {
-    cont.push(
-    <div className={styles.griditem} key={item.name}>
-      <img className={styles.image} src={item.avatar_url}></img>
-    <a href={item.profile}>{item.name}</a>
-    </div>
-    )
-  });
-  return cont;
 }
 export default ContributorShowcase;
