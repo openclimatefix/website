de8ba8a0198dd64c73a12a72f94947939ba5261f
drwm-base <david.medeiros@live.com>
2019-12-04 09:23:01 -0500

    fixed more syntax issues

--- src/pages/get-involved.jsx
+++ src/pages/get-involved.jsx
@@ -8,31 +8,32 @@ const getInvolvedPage = () => (
     <div className="markdown">
       <ul>
         <li>
-            <a href="https://opencollective.com/openclimatefix">Donate</a>
-            {' '}
-            to our OpenCollective
-          </li>
-          <li>
-            Follow us by
-            {' '}
-            <a href="https://eepurl.com/guCjvH">signing up to our newsletter</a>
-          </li>
-          <li>
-            Join the
-            {' '}
-            <a href="https://openclimatefix.discourse.group/">OCF discussion forum</a>
-          </li>
-        </ul>
-        <p>
-          Also check out
+          <a href="https://opencollective.com/openclimatefix">Donate</a>
           {' '}
-          <a href="/blog/2019-03-06-how-to-get-involved">
-            Jack&apos;s blog post about getting involved
-          </a>
-          .
-        </p>
+          to our OpenCollective
+        </li>
+        <li>
+          Follow us by
+          {' '}
+          <a href="https://eepurl.com/guCjvH">signing up to our newsletter</a>
+        </li>
+        <li>
+          Join the
+          {' '}
+          <a href="https://openclimatefix.discourse.group/">OCF discussion forum</a>
+        </li>
+      </ul>
+      <p>
+        Also check out
+        {' '}
+        <a href="/blog/2019-03-06-how-to-get-involved">
+          Jack&apos;s blog post about getting involved
+        </a>
+        .
+      </p>
       <h2>Contribute to one of our repositories on Github</h2>
-      <p>Check out our 
+      <p>
+        Check out our 
         <a href="https://github.com/search?l=&p=1&q=user%3Aopenclimatefix+label%3A%22good+first+issue%22&ref=advsearch&type=Issues&utf8=%E2%9C%93&state=open">List of Good First Issues</a>
         to get started!
       </p>
