7ef842a73660fb73c7f503d6da19e73d6e787b69
Jack Kelly <jack@OpenClimateFix.org>
2020-01-09 20:42:42 +0000

    :speech_balloon: Update text about PV nowcasting

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -10,25 +10,25 @@ const ProjectsPage = () => (
     <h1>Projects</h1>
     <main className="markdown">
       <p>
-        Currently we are pursuing two projects at OCF, both are focused on Solar
+        We are currently pursuing two projects at OCF, both are focused on Solar
         Photovoltaics (PV).
       </p>
       <p>
         Our first project,&nbsp;
         <Link to="/projects/nowcasting">Solar PV Nowcasting</Link>
-        , investigates the possibilities of using Machine Learning Models to forecast
+        , investigates the possibilities of using Machine Learning to forecast
         the output of solar photovoltaic installations for the next few hours.
       </p>
       <p>
-        Output of PV is a very uncertain measure: Clouds are moving constantly and
-        are influencing electricity generation heavily. To keep the energy grid in
-        balance, operators need to have a readily available backup which usually
-        comes from fossile sources.
+        Solar PV power production is a hard to forecast: As clouds move over PV panels,
+        the power output moves up and down rapidly. To keep the energy grid in
+        balance, electricity grid operators need to have a readily available backup
+        which usually comes from fossil sources.
       </p>
       <p>
         Our assumption is the following: If we have more accurate predictions of
-        how much electricity a PV installation will produce in a few hours from
-        now, less time can be spend on running fossile fuel backups.
+        how much electricity a PV installation will produce over the next few hours then we
+        can reduce the amount of fossil fuel backup required.
       </p>
       <p>
         <Link to="/blog/2019-07-09-solar-pv-mapping">PV Mapping&nbsp;</Link>
