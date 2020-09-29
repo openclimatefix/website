c9c951c59ad78a732230f20b13ee8ca48583cf07
dantravers <38562875+dantravers@users.noreply.github.com>
2020-09-29 22:57:55 +0100

    Change from PV mapping to Solar panel mapping

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -55,7 +55,7 @@ const ProjectsPage = () => (
         </Link>
       </p>
 
-      <h2 id="pv-mapping">3. Photovoltaic Mapping</h2>
+      <h2 id="pv-mapping">3. Solar Panel Mapping</h2>
       <p>
         To accurately forecast solar power generation, we need to know where all
         the PV panels are. That is still largely unknown today in the UK. We are
@@ -69,7 +69,7 @@ const ProjectsPage = () => (
       </p>
       <p>
         <Link to="/projects/pv-mapping">
-          &gt; Learn more about Photovoltaic Mapping
+          &gt; Learn more about Solar Panel Mapping
         </Link>
       </p>
     </main>
