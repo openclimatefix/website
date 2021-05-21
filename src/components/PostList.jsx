0e1257bdf24d0c636d4a98ab1f4555b68b761ec9
how melnyczuk <how@melnycz.uk>
2021-05-21 10:51:08 +0200

    Add PostFilters component and use it in PostList

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import PostLink from './PostLink';
+import PostFilters from './PostFilters';
 
-const PostList = ({ posts }) => (
-  <div className="mt-6">
-    <ul>
-      {posts.map(({ node: post }) => (
-        <PostLink key={post.id} post={post} />
-      ))}
-    </ul>
-  </div>
-);
+const PostList = ({ posts }) => {
+  const [projectFilter, setProjectFilter] = useState();
+
+  const projects = useMemo(
+    () =>
+      posts.reduce((acc, { node: post }) => {
+        const { project } = post.frontmatter;
+        return !!project && !acc.includes(project) ? [...acc, project] : acc;
+      }, []),
+    [posts]
+  );
+
+  return (
+    <div className="mt-6">
+      <PostFilters
+        options={projects}
+        selected={projectFilter}
+        setSelected={setProjectFilter}
+      />
+      <ul>
+        {posts
+          .filter(
+            ({ node: post }) =>
+              !projectFilter || projectFilter === post.frontmatter.project
+          )
+          .map(({ node: post }) => (
+            <PostLink key={post.id} post={post} />
+          ))}
+      </ul>
+    </div>
+  );
+};
 
 PostList.propTypes = {
   posts: PropTypes.arrayOf(
     PropTypes.shape({
       node: PropTypes.shape({
         id: PropTypes.string.isRequired,
+        frontmatter: {
+          project: PropTypes.string,
+        },
       }).isRequired,
     }).isRequired
   ).isRequired,
--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -55,6 +55,7 @@ export const pageQuery = graphql`
             authorName
             authorImage
             coverImageUnsplashId
+            project
           }
           fields {
             readingTime {
