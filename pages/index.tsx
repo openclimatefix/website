0621333b79afdff5dafd36fabbd38f1573f4cd66
flowirtz <6052785+flowirtz@users.noreply.github.com>
2021-11-02 11:58:07 +0000

    ✨  Create Newsletter section

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,8 @@ import cloudsBackground from "../public/backgrounds/clouds.jpg";
 import ocfLogoWhite from "../public/logos/ocf_logo_white.svg";
 import Footer from "../components/footer";
 
+import { ArrowRightIcon } from "@heroicons/react/solid";
+
 const cloudflareImageLoader = ({
   src,
   width,
@@ -70,7 +72,43 @@ const Home: NextPage = () => {
         <section className="w-screen h-screen p-10 text-white bg-ocf-black-100">
           TODO
         </section>
-        <section className="bg-yellow-400 h-72 w-full">Newsletter</section>
+        <section className="bg-yellow-400 w-full px-6 py-10">
+          <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-x-8">
+            <div>
+              <p className="text-2xl font-medium text-ocf-black-100 max-w-lg">
+                Join our mailing list for updates about our projects, research,
+                and more.
+              </p>
+              <p className="text-xs leading-relaxed mt-2 md:ml-0 mb-4 text-ocf-black-100">
+                No spam, we promise. Read our{" "}
+                <a href="#" className="underline">
+                  Privacy Policy
+                </a>
+                .
+              </p>
+            </div>
+            <form className="mt-8 mb-2 md:my-auto">
+              <div className="">
+                <label htmlFor="mce-EMAIL" className="sr-only">
+                  Email address
+                </label>
+                <div className="relative max-w-md md:max-w-full">
+                  <input
+                    type="text"
+                    className="focus:ring-black focus:border-black block w-full rounded-full border-white py-4 px-6 pr-14"
+                    placeholder="Email"
+                  />
+                  <button className="absolute inset-y-0 right-0 pr-6 flex items-center pointer-events-none">
+                    <ArrowRightIcon
+                      className="h-5 w-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </section>
       </main>
       <Footer />
     </div>
