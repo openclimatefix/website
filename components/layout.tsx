a53cdd0b5469da1d96a17326ef579ecab0906aab
flowirtz <6052785+flowirtz@users.noreply.github.com>
2022-01-20 15:09:07 +0000

    Add meta headers

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,10 +7,12 @@ import PageHeader, { IPageHeader } from "./pageHeader";
 interface ILayout extends IPageHeader {
   children: React.ReactNode;
   isTextPage?: boolean;
+  description: string;
 }
 
 const Layout = ({
   children,
+  description,
   isTextPage = true,
   ...pageHeaderProps
 }: ILayout) => {
@@ -18,7 +20,34 @@ const Layout = ({
     <div className="bg-white">
       <Head>
         <title>{pageHeaderProps.title} | Open Climate Fix</title>
-        {/* <meta property="og:title" content="My page title" key="title" /> */}
+        <meta
+          property="og:title"
+          content={pageHeaderProps.title}
+          key="ogtitle"
+        />
+        <meta name="description" content={description} />
+        <meta property="og:description" content={description} key="ogdesc" />
+
+        {/* Twitter */}
+        {/* https://cards-dev.twitter.com/validator */}
+        <meta name="twitter:card" content="summary" key="twcard" />
+        <meta name="twitter:creator" content="openclimatefix" key="twhandle" />
+
+        {/* Open Graph */}
+        {/* https://ogp.me/ */}
+        {/* <meta property="og:url" content={currentURL} key="ogurl" /> */}
+        {/* <meta property="og:image" content={pageHeaderProps.titleImage} key="ogimage" /> */}
+        <meta
+          property="og:site_name"
+          content={"Open Climate Fix"}
+          key="ogsitename"
+        />
+        <meta
+          property="og:title"
+          content={pageHeaderProps.title}
+          key="ogtitle"
+        />
+        <meta property="og:description" content={description} key="ogdesc" />
       </Head>
       <PageHeader {...pageHeaderProps} />
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,34 +1,12 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import GenericMetaHeaders from "../components/meta-headers";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
-          <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-          <meta name="theme-color" content="#141006" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
-            rel="stylesheet"
-          />
+          <GenericMetaHeaders />
         </Head>
         <body className="bg-ocf-black-100">
           <Main />
