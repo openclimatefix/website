8f511045157c0f5e05ecea88aaf9d71df9c1e562
flowirtz <6052785+flowirtz@users.noreply.github.com>
2022-01-20 19:00:09 +0000

    Remove experimental features

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  experimental: {
-    urlImports: [
-      "https://framer.com/m/",
-      "https://framerusercontent.com/",
-      "https://ga.jspm.io/",
-      "https://jspm.dev/",
-    ],
-  },
 };
