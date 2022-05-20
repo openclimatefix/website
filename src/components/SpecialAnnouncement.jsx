6e5690d542a3cacc39ef09128f7372ea43e65daa
Flo <6052785+flowirtz@users.noreply.github.com>
2022-05-20 14:20:59 +0100

    Display hiring banner on homepage

--- src/components/SpecialAnnouncement.jsx
+++ src/components/SpecialAnnouncement.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function SpecialAccouncement() {
   return (
-    <a href="https://apply.workable.com/openclimatefix">
+    <a href="https://openclimatefix.homerun.co/fullstack-engineer">
       <div style={{ backgroundColor: '#FFC425' }}>
         <div className="max-w-7xl mx-auto py-2 px-3 sm:px-6 lg:px-8 container mx-auto">
           <p className="ml-3 font-medium text-black text-center">We are hiring!</p>
